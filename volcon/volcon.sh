b6fe137ca7682898dfbe64f94e60e9ce6c35f332
suve <veg@svgames.pl>
2015-08-18 22:44:51 +0200

    Add option support to volcon (-d, -c and -m)

--- volcon/volcon.sh
+++ volcon/volcon.sh
@@ -2,13 +2,32 @@
 
 DEVICE='default'
 CONTROL='Master'
+MICROPHONE='Capture'
 
-USAGE1="Usage: volcon <inc|dec|set> <value>"
-USAGE2="       volcon <get|mute|unmute|toggle>"
+USAGE=$(cat <<EOT
+Usage: volcon [-d device] [-c control] [-m] <inc|dec|set> <value>
+       volcon [-d device] [-c control] [-m] <get|mute|unmute|toggle>
+  -d  Specify the amixer device to use. Defaults to '$DEVICE'.
+  -c  Specify the amixer control to use. Defaults to '$CONTROL'.
+  -m  Use the microphone control. Equivalent to -c '$MICROPHONE'.
+EOT
+);
+
+# Use getopts to check for -options
+while getopts 'd:c:m' OPTNAME; do
+   if [ "$OPTNAME" == 'd' ]; then
+      DEVICE="OPTARG"
+   elif [ "$OPTNAME" == 'c' ]; then
+      CONTROL="$OPTARG"
+   elif [ "$OPTNAME" == 'm' ]; then
+      CONTROL="$MICROPHONE"
+   fi
+done
+# Shift -options and start the script proper
+shift $((OPTIND-1))
 
 if [ "$#" -lt 1 ]; then
-   echo "$USAGE1"
-   echo "$USAGE2"
+   echo "$USAGE"
    exit
 fi
 
@@ -35,8 +54,7 @@ else
 fi
 
 if [ "$SUFFIX" == '' ] || [ "$#" -lt 2 ]; then
-   echo "$USAGE1"
-   echo "$USAGE2"
+   echo "$USAGE"
    exit
 fi
 
