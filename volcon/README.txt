5e686cf51650222a9b22630580a3906485edc27c
suve <veg@svgames.pl>
2015-08-18 23:36:01 +0200

    Add desktop notifications to volcon and update readme

--- volcon/README.txt
+++ volcon/README.txt
@@ -1,7 +1,13 @@
 volcon
 ========================================
-Usage: volcon <get|mute|unmute|toggle>
-       bricon <inc|dec|set> <value>
+Usage: volcon [-d device] [-c control] [-m] <inc|dec|set> <value>
+       volcon [-d device] [-c control] [-m] <get|mute|unmute|toggle>
+  -d  Specify the amixer device to use. Defaults to 'default'.
+  -c  Specify the amixer control to use. Defaults to 'Master'.
+  -m  Use the microphone control. Equivalent to -c 'Capture'.
+  -n  Display a desktop notification with control status and volume.
+      Requires 'notify-send' present on the system.
+  -q  Do not print amixer output.
 
 When called with the "get" parameter, volcon returns the current
 audio volume. "mute" and "unmute" are self-explanatory. "toggle"
@@ -16,10 +22,15 @@ the current level.
 
 ----------------------------------------
 
-volcon uses amixer to perform it's duty, to it will only work on
+volcon uses amixer to perform its duty, so it will only work on
 systems which use ALSA for audio handling.
 
-volcon always operates on the default audio device, on the master
-channel. Although this can be changed via editing the variables
-at the beginning of the script, there is currently no support
-for specifying the device/channel via arguments.
+By default, volcon operates on the default audio device, on the master
+channel. The -d, -c and -m options can be used to operate on different
+devices or channels. If you find that the default devices/controls do
+not work for you, these can be easily changed by editing the variables
+at the beginning of the script.
+
+If you have notify-send present on the system (and a notification server
+running), the -n option can be used to display a desktop notification
+with the control status (audible/mute) and volume level.
--- volcon/volcon.sh
+++ volcon/volcon.sh
@@ -5,22 +5,52 @@ CONTROL='Master'
 MICROPHONE='Capture'
 
 USAGE=$(cat <<EOT
-Usage: volcon [-d device] [-c control] [-m] <inc|dec|set> <value>
-       volcon [-d device] [-c control] [-m] <get|mute|unmute|toggle>
+Usage: volcon [-d device] [-c control] [-mnq] <inc|dec|set> <value>
+       volcon [-d device] [-c control] [-mnq] <get|mute|unmute|toggle>
   -d  Specify the amixer device to use. Defaults to '$DEVICE'.
   -c  Specify the amixer control to use. Defaults to '$CONTROL'.
   -m  Use the microphone control. Equivalent to -c '$MICROPHONE'.
+  -n  Display a desktop notification with control status and volume.
+      Requires 'notify-send' present on the system.
+  -q  Do not print amixer output.
 EOT
 );
 
+function volcon() {
+   OUTPUT=`amixer $@`
+   
+   if [ "$QUIET" == '' ]; then
+      echo "$OUTPUT"
+   fi
+   
+   if [ "$NOTIFY" != '' ]; then
+      LEVELS=`echo "$OUTPUT" | grep -o -e '\[[[:digit:]]*%\]'`
+      NUM=`echo "$LEVELS" | wc -l`
+      SUM=`echo "$LEVELS 0" | tr '[%]\n' '  + ' | xargs expr`
+      AVG=`expr $SUM / $NUM`
+      
+      if [ `echo "$OUTPUT" | grep -c -e '\[on\]'` -gt 0 ]; then
+         STATUS="$AVG%"
+      else
+         STATUS="$AVG%  [MUTE]"
+      fi
+      
+      notify-send -a 'volcon' -t 1500 "$CONTROL: $STATUS"
+   fi
+}
+
 # Use getopts to check for -options
-while getopts 'd:c:m' OPTNAME; do
+while getopts 'd:c:mnq' OPTNAME; do
    if [ "$OPTNAME" == 'd' ]; then
       DEVICE="OPTARG"
    elif [ "$OPTNAME" == 'c' ]; then
       CONTROL="$OPTARG"
    elif [ "$OPTNAME" == 'm' ]; then
       CONTROL="$MICROPHONE"
+   elif [ "$OPTNAME" == 'n' ]; then
+      NOTIFY=1
+   elif [ "$OPTNAME" == 'q' ]; then
+      QUIET=1
    fi
 done
 # Shift -options and start the script proper
@@ -32,12 +62,12 @@ if [ "$#" -lt 1 ]; then
 fi
 
 if [ "$1" == 'get' ]; then
-   amixer -D "$DEVICE" get "$CONTROL"
+   volcon -D "$DEVICE" get "$CONTROL"
    exit
 fi
 
 if [ "$1" == 'mute' ] || [ "$1" == 'unmute' ] || [ "$1" == 'toggle' ]; then
-   amixer -D "$DEVICE" set "$CONTROL" "$1"
+   volcon -D "$DEVICE" set "$CONTROL" "$1"
    exit
 fi
 
@@ -58,5 +88,5 @@ if [ "$SUFFIX" == '' ] || [ "$#" -lt 2 ]; then
    exit
 fi
 
-amixer -D "$DEVICE" set "$CONTROL" "$2$SUFFIX"
+volcon -D "$DEVICE" set "$CONTROL" "$2$SUFFIX"
 
